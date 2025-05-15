db0e74e0d54ac25edddc0775ee5ba78c9dfc91ec
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2025-05-14 21:13:56 -0500

    Routers añadidos al main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from routers import libros
+from routers import libros, revistas, dvds
 
 app = FastAPI()
 
 app.include_router(libros.router)
+app.include_router(revistas.router)
+app.include_router(dvds.router)
